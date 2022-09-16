101145c0b65d9aecbd6c56dbda3f229dba564f3a
ongweisheng <ongweisheng30@gmail.com>
2022-09-16 21:19:04 +0800

    Set up connection to DB and create health check

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import "dotenv/config"
+
+const uri = process.env.DB_URL
+
+mongoose.connect(uri)
+    .then((connections) => {
+        console.log(`Database connection succeeded! Database name: ${connections.connections[0].name}`)
+    }).catch((err) => {
+        console.log(err)
+        throw new Error("Database connection failed")
+    })
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "ok",
+        descript: "this is just health check"
+    })
+})
+
+app.listen(port, () => {
+    console.log("Server started on port: ", port)
+})
+
+export default app
